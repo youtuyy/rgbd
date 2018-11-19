f7c460616dcf728b2d009433e6dbb10179d8e6bf
youtuyy <linzhaoyan@hotmail.com>
2018-11-18 16:32:34 -0800

    update

--- desk1/temp.py
+++ desk1/temp.py
@@ -2,14 +2,14 @@ import sys
 import os
 
 i = 1
-with open("depth.txt", "w") as a:
-    for path, subdirs, files in os.walk(r'./depth'):
+with open("rgb.txt", "w") as a:
+    for path, subdirs, files in os.walk(r'./rgb'):
 	files.sort()  
         for filename in files:
             f = os.path.join(path, filename)
 	    if i % 5 == 1:
                 # f = os.path.join(path, filename)
-                a.write(str(filename)[:-4] + " " + str(f)[1:] + os.linesep)
+                a.write(str(filename)[:-4] + " " + str(f)[2:] + os.linesep)
 	    else:
 	        os.remove(f) 
             i = i + 1
